dba07449c8e5a7960c690e26d03c72ec3700e0b9
junya <97082343+ynurb@users.noreply.github.com>
2023-03-08 00:28:23 -0500

    Create credentials.py

---
+++ credentials.py
@@ -0,0 +1,10 @@
+# Imports
+import os
+
+# CLIENT_ID & CLIENT_SECRET set in environment variables (held in .zshrc)
+def get_client_id() -> str:
+    return os.environ["CLIENT_ID"]
+
+def get_client_secret() -> str:
+    return os.environ["CLIENT_SECRET"]
+
