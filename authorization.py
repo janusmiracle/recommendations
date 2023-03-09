5d2566f0fe3a313bf1be66db3018aa3c918a8059
junya <97082343+ynurb@users.noreply.github.com>
2023-03-08 19:19:21 -0500

    Update authorization.py

--- authorization.py
+++ authorization.py
@@ -1,41 +1,46 @@
-# Import client and client_secret
+# Project imports
 from credentials import get_client_id, get_client_secret
-# Other imports
+
+# For all imports, sort `import` first then `from`s, in alphabetical order
+# Python standard library imports
+import json
 import base64
 from urllib.parse import urlencode
+
+# Third-party imports
 import requests
-import json
+
 
 # Query Parameters // Set Client ID and Client Secret
 CLIENT_ID = get_client_id()
 CLIENT_SECRET = get_client_secret()
 
-# Client Credential Flow
-def authorization(CLIENT_ID, CLIENT_SECRET) -> json:
-     # auth_code for header
-     auth_code = base64.b64encode((CLIENT_ID + ":" + CLIENT_SECRET).encode("ascii")).decode("ascii")
 
-     headers = {
-          "Content-Type": "application/x-www-form-urlencoded",
-          "Authorization": "Basic " + auth_code
-     }
-     # payload
-     payload = {
-          "grant_type": "client_credentials"
-     }
+def get_access_token(CLIENT_ID, CLIENT_SECRET) -> json:
+    # auth_code for header
+    auth_code = base64.b64encode(
+        (CLIENT_ID + ":" + CLIENT_SECRET).encode("ascii")
+    ).decode("ascii")
 
-     # post request
-     response = requests.post("https://accounts.spotify.com/api/token", data=payload, headers=headers)
-     # json data
-     data = response.json()
-     formatted_data = json.dumps(response.json(), indent=4) # formats into example from Spotify documentation
-     # access token
-     token = data['access_token']
-      
-     return token
+    headers = {
+        "Content-Type": "application/x-www-form-urlencoded",
+        "Authorization": "Basic " + auth_code,
+    }
+    # payload
+    payload = {"grant_type": "client_credentials"}
 
-# Call function to obtain access token
-access_token = authorization(CLIENT_ID, CLIENT_SECRET)
+    # post request
+    response = requests.post(
+        "https://accounts.spotify.com/api/token", data=payload, headers=headers
+    )
+    # json data
+    data = response.json()
+    formatted_data = json.dumps(
+        response.json(), indent=4
+    )  # formats into example from Spotify documentation
+    # access token
+    token = data["access_token"]
+    return token
 
 
-
+access_token = get_access_token(CLIENT_ID, CLIENT_SECRET)
